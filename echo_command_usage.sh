e6f97b16f8d7a5939fa58996ebf43a2677ba6ea4
phuepwintthaekhaing <phuepwintthaekhaing17897@gmail.com>
2022-05-13 14:37:34 +0630

    Create shell file

---
+++ echo_command_usage.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# My first script
+
+echo "Hello World!"
+echo this is a test
+
+#Arithmetic Expansion
+echo $((2 + 2))
+echo $(($((5**2)) * 3))
+echo $(((5**2) * 3))
+
+#Brace Expansion
+echo Front-{A,B,C}-Back
+echo Number_{1..5}
+echo a{A{1,2},B{3,4}}b
+
+#Quoting
+echo this is a     test
+echo The total is $100.00
+
+#Double quote
+echo "$(cal)"
+
+#Single quote
+echo 'text ~/*.txt {a,b} $(echo foo) $((2+2)) $USER'
+
+
+
+
+

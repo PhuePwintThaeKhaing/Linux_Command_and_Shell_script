4f398c058e9a19d3011a30486c0d3848ca76bed8
phuepwintthaekhaing <phuepwintthaekhaing17897@gmail.com>
2022-05-14 09:55:41 +0630

    Update echo_command_usage.sh

--- bootcamp/echo_command_usage.sh
+++ bootcamp/echo_command_usage.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
 # My first script
-
-echo "Hello World!"
-echo this is a test
-
-#Arithmetic Expansion
-echo $((2 + 2))
-echo $(($((5**2))*3))
-echo $(((5**2)*3))
-
-#Brace Expansion
-echo Number_{1..5}
-echo a{A{1,2},B{3,4}}b
-
-#Quoting
-echo "this is a     test"
-echo The total is $100.00
-
-#Double quote
-echo "calendar"
-
-#Single quote
-echo 'text ~/*.txt {a,b} $(echo foo) $((2+2)) $USER'
+echo "<html>
+ <head>
+   <title>
+   Shell Script Practice
+   </title>
+ </head>
+ 
+ <body>
+   Learn echo command and use to code shell script file.
+ </body>
+ </html>"
